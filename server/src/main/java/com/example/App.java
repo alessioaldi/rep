e3d3dc0566e6e0a92a1ee5a227aabf7373a766f9
alessioaldi <aldinucci.alessio@itismeucci.com>
2023-10-09 09:21:07 +0200

    server finito

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -1,5 +1,12 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +15,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            System.out.println("Server avviato");
+            ServerSocket server = new ServerSocket(3000);
+            Socket s = server.accept();
+            System.out.println("in client si è ocnnesso");
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            String stringaRicevuta = in.readLine();
+            System.out.println("il client ha inviato" + stringaRicevuta);
+            String stringaModificata = stringaRicevuta.toUpperCase();
+            out.writeBytes(stringaModificata + "\n");
+            server.close();
+            s.close();
+           
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
     }
 }
