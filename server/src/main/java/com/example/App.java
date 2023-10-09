80943f2dd580e7543bc9fd649f6641b1034b9b8f
alessioaldi <aldinucci.alessio@itismeucci.com>
2023-10-09 10:23:44 +0200

    server non chiude

--- server/src/main/java/com/example/App.java
+++ server/src/main/java/com/example/App.java
@@ -16,25 +16,32 @@ public class App
 
     public static void main( String[] args )
     {
+        String stringa=null;
+        while(stringa.equals("q")==false){
+            try {
+                System.out.println("Server avviato");
+                ServerSocket server = new ServerSocket(3000);
+                Socket s = server.accept();
 
-        try {
-            System.out.println("Server avviato");
-            ServerSocket server = new ServerSocket(3000);
-            Socket s = server.accept();
-
-            System.out.println("in client si è ocnnesso");
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            String stringaRicevuta = in.readLine();
-            System.out.println("il client ha inviato" + stringaRicevuta);
-            String stringaModificata = stringaRicevuta.toUpperCase();
-            out.writeBytes(stringaModificata + "\n");
-            server.close();
-            s.close();
-           
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.exit(1);
+                System.out.println("in client si è ocnnesso");
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                DataOutputStream out = new DataOutputStream(s.getOutputStream());
+                String stringaRicevuta = in.readLine();
+                stringa = stringaRicevuta;
+                System.out.println("il client ha inviato" + stringaRicevuta);
+                String stringaModificata = stringaRicevuta.toUpperCase();
+                if(stringa.equals("q")){
+                    out.writeBytes("yes");
+                } else{
+                out.writeBytes(stringaModificata + "\n");
+                }
+                server.close();
+                s.close();
+            
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.exit(1);
+            }
         }
     }
 }
